9b3bab1e77764b3f2a3d20d53b0177fc5e92051e
Yuriy Litvinenko <mailto:jurij.litvinenko@gmail.com>
2018-06-13 14:29:08 +0600

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,12 @@
 package ru.job4j.calculate;
 
+/** Class for print "Hello World" */
 public class Calculate {
 	
+	/** Method for print "Hello World" */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	
 }
+
