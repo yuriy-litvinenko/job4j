e3e6a87a655da9ecaee604314eccbe74e2adc420
Yuriy Litvinenko <mailto:jurij.litvinenko@gmail.com>
2018-06-20 13:21:02 +0600

    4.4. Максимум из трех чисел

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -5,4 +5,10 @@ public class Max {
     public int max(int first, int second) {
         return first > second ? first : second;
     }
+
+    public int max(int first, int second, int third) {
+        int temp = this.max(first, second);
+        temp = this.max(temp, third);
+        return temp;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MaxTest {
+
+    //Проверка метода вычисления максимума из двух чисел
     @Test
     public void whenFirstLessSecond() {
         Max maxim = new Max();
@@ -24,5 +26,27 @@ public class MaxTest {
         Max maxim = new Max();
         int result = maxim.max(2, 2);
         assertThat(result, is(2));
+    }
+
+    //Проверка метода вычисления максимума из трех чисел
+    @Test
+    public void whenFirstLargerOther() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenSecondLargerOther() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 3, 2);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenThirdLargerOther() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 1, 3);
+        assertThat(result, is(3));
     }
 }
