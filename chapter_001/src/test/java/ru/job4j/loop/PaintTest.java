e64d9b7094f2d50544bf78855719ae231f2a5ef4
Yuriy Litvinenko <mailto:jurij.litvinenko@gmail.com>
2018-06-21 15:02:27 +0600

    5.4. Построить пирамиду в псевдографике.

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class PaintTest {
+
+    @Test
+    public void whenPyramid3Height() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(3);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("  ^  ")
+                                .add(" ^^^ ")
+                                .add("^^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4Height() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
