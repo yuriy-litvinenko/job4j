8155edbef456308299f2640778cee20e9cc74ac0
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2018-11-08 10:49:30 +0600

    Кофемашина. Тестовое задание (исправление 1)

--- chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
@@ -3,25 +3,20 @@ package ru.job4j.coffee;
 public class CoffeeMachine {
 
     public int[] changes(int value, int price) {
-        int coin10, coin5, coin2, coin1;
         int change = value - price;
-        coin10 = change / 10;
-        coin5 = (change - coin10 * 10) / 5;
-        coin2 = (change - coin10 * 10 - coin5 * 5) / 2;
-        coin1 = change - coin10 * 10 - coin5 * 5 - coin2 * 2;
-        int size = coin10 + coin5 + coin2 + coin1;
-        int[] coins = new int[size];
-        for (int index = 0; index <= coins.length; index++) {
-            if (coin10-- > 0) {
-                coins[index] = 10;
-            } else if (coin5-- > 0) {
-                coins[index] = 5;
-            } else if (coin2-- > 0) {
-                coins[index] = 2;
-            } else if (coin1-- > 0) {
-                coins[index] = 1;
+        int[] coins = {10, 5, 2, 1};
+        int coinsCnt = 0;
+        int[] result = new int[coinsCnt];
+        for (int coin : coins) {
+            while (change >= coin) {
+                change -= coin;
+                coinsCnt++;
+                int[] arrTmp = new int[coinsCnt];
+                System.arraycopy(result, 0, arrTmp, 0, result.length);
+                arrTmp[arrTmp.length - 1] = coin;
+                result = arrTmp;
             }
         }
-        return coins;
+        return result;
     }
 }
