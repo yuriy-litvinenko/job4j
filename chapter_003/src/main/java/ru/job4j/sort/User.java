932bd5859cf8836a10f516e2e03cded43a2ca7cc
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2018-11-14 16:33:00 +0600

    2. Сортировка User с использованием Comparator

--- chapter_003/src/main/java/ru/job4j/sort/User.java
+++ chapter_003/src/main/java/ru/job4j/sort/User.java
@@ -9,6 +9,14 @@ public class User implements Comparable<User> {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     @Override
     public int compareTo(User o) {
         return Integer.compare(this.age, o.age);
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -12,17 +12,56 @@ import static org.junit.Assert.assertThat;
 
 public class SortUserTest {
     @Test
-    public void SortUsersTreeSetByAge() {
+    public void sortUsersTreeSetByAge() {
         List<User> users = new ArrayList<>();
         SortUser sortUser = new SortUser();
-        User user1 = new User("Ivan", 30);
-        User user2 = new User("Stepan", 20);
-        User user3 = new User("Ravshan", 25);
+        User user1 = new User("Ravshan", 30);
+        User user2 = new User("Ivan", 20);
+        User user3 = new User("Stepan", 25);
         users.add(user1);
         users.add(user2);
         users.add(user3);
         Set<User> result = sortUser.sort(users);
         Set<User> expect = new TreeSet<>();
+        expect.add(user2);
+        expect.add(user3);
+        expect.add(user1);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void sortListByNameLengthComparator() {
+        List<User> users = new ArrayList<>();
+        SortUser sortUser = new SortUser();
+        User user1 = new User("Ravshan", 30);
+        User user2 = new User("Ivan", 20);
+        User user3 = new User("Stepan", 25);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        List<User> result = sortUser.sortNameLength(users);
+        List<User> expect = new ArrayList<>();
+        expect.add(user2);
+        expect.add(user3);
+        expect.add(user1);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void sortListByNameAndAgeComparator() {
+        List<User> users = new ArrayList<>();
+        SortUser sortUser = new SortUser();
+        User user1 = new User("Sergey", 25);
+        User user2 = new User("Ivan", 30);
+        User user3 = new User("Sergey", 20);
+        User user4 = new User("Ivan", 25);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        List<User> result = sortUser.sortByAllFields(users);
+        List<User> expect = new ArrayList<>();
+        expect.add(user4);
         expect.add(user2);
         expect.add(user3);
         expect.add(user1);
