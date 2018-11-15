f1865a864fe143de3909afe0da88d583dca4beab
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2018-11-15 16:44:03 +0600

    1. JaCoCo. Процент покрытие тестами.

--- chapter_001/src/main/java/ru/job4j/array/ArraySort.java
+++ chapter_001/src/main/java/ru/job4j/array/ArraySort.java
@@ -3,14 +3,15 @@ package ru.job4j.array;
 /**
  * Контрольное задание.
  * Заполнение массива данными из двух других отсортированных массивов с соблюдением сортировки.
+ *
  * @author Юрий Литвиненко
  * @since 03.07.2018
  */
 public class ArraySort {
 
-    public int[] ArraySumSort (int ar1[], int ar2[]) {
-        int ar3[] = new int[ar1.length+ar2.length];
-        for (int i1 = 0, i2 =0, i3 = 0; i3 < ar3.length; i3++){
+    public int[] arraySumSort(int[] ar1, int[] ar2) {
+        int[] ar3 = new int[ar1.length + ar2.length];
+        for (int i1 = 0, i2 = 0, i3 = 0; i3 < ar3.length; i3++) {
             if (i1 == ar1.length) {
                 ar3[i3] = ar2[i2++];
             } else if (i2 == ar2.length) {
