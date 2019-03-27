5af7988c3e214edd3cd104256819c3fca04a078e
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-03-28 02:05:08 +0600

    3. Уникальность элементов и сортировка. [#110225]

--- chapter_004/src/main/java/ru/job4j/addresses/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/addresses/Profiles.java
@@ -1,10 +1,13 @@
 package ru.job4j.addresses;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream().map(Profile::getAddress).sorted(
+                Comparator.comparing(Address::getCity)
+        ).distinct().collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/addresses/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/addresses/ProfilesTest.java
@@ -10,15 +10,17 @@ import static org.junit.Assert.assertThat;
 public class ProfilesTest {
 
     @Test
-    public void getAddressesListFromProfilesList() {
-        Address address1 = new Address("Moscow", "Pushkina", 10, 125);
+    public void getUniqueAndSortedListOfAddressesFromListOfProfiles() {
+        Address address1 = new Address("Samara", "Lermontova", 22, 250);
         Address address2 = new Address("Penza", "Malahova", 3, 55);
-        Address address3 = new Address("Samara", "Lermontova", 22, 250);
+        Address address3 = new Address("Moscow", "Pushkina", 10, 125);
         Profile profile1 = new Profile(address1);
         Profile profile2 = new Profile(address2);
         Profile profile3 = new Profile(address3);
-        List<Address> result = new Profiles().collect(List.of(profile1, profile2, profile3));
-        List<Address> expect = List.of(address1, address2, address3);
+        Profile profile4 = new Profile(address3);
+        Profile profile5 = new Profile(address2);
+        List<Address> result = new Profiles().collect(List.of(profile1, profile2, profile3, profile4, profile5));
+        List<Address> expect = List.of(address3, address2, address1);
         assertThat(result, is(expect));
     }
 }
