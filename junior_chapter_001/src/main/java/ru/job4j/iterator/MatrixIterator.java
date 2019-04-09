576e3b83f0f9bbc27abbf4d969fca98ce506b769
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-04-10 02:18:25 +0600

    5.1.1. Итератор для двухмерного массива int[][][#131959]

---
+++ junior_chapter_001/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,32 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+public class MatrixIterator implements Iterator {
+
+    private final int[][] values;
+    private int i = 0, j = 0;
+
+    public MatrixIterator(final int[][] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return values.length - 1 > i;
+    }
+
+    @Override
+    public Object next() {
+        if (values[i].length > j) {
+            return values[i][j++];
+        } else {
+            i++;
+            j = 0;
+            if (values.length > i) {
+                return values[i][j++];
+            }
+        }
+        return null;
+    }
+}
