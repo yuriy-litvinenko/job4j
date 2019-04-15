7a613d59c56ba0dcfd49c0a3a0244d0a8f3b0a43
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-04-16 01:26:00 +0600

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack[#131949]

---
+++ junior_chapter_001/src/main/java/ru/job4j/list/SimpleStack.java
@@ -0,0 +1,34 @@
+package ru.job4j.list;
+
+import java.util.NoSuchElementException;
+
+public class SimpleStack<T> {
+    private int size;
+    private Node<T> first;
+
+    public T poll() {
+        if (size > 0) {
+            Node<T> pollLink = this.first;
+            this.first = this.first.next;
+            this.size--;
+            return pollLink.date;
+        }
+        throw new NoSuchElementException();
+    }
+
+    public void push(T value) {
+        Node<T> newLink = new Node<>(value);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    private static class Node<T> {
+        T date;
+        Node<T> next;
+
+        Node(T date) {
+            this.date = date;
+        }
+    }
+}
