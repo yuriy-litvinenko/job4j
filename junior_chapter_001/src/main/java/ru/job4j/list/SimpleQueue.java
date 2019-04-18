bf90bc090d3708a06a42f4eed1d8a4dc0511c0ba
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-04-19 00:49:31 +0600

    5.3.3.1 Очередь на двух стеках[#131952]

--- junior_chapter_001/src/main/java/ru/job4j/list/SimpleQueue.java
+++ junior_chapter_001/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -2,27 +2,23 @@ package ru.job4j.list;
 
 public class SimpleQueue<T> {
 
-    private LinkedListContainer<SimpleStack<T>> stackList = new LinkedListContainer<>();
-
-    public SimpleQueue() {
-        stackList.add(new SimpleStack<>());
-        stackList.add(new SimpleStack<>());
-    }
+    private SimpleStack<T> in = new SimpleStack<>();
+    private SimpleStack<T> out = new SimpleStack<>();
 
     public T poll() {
-        if (stackList.get(1).getSize() == 0) {
+        if (out.getSize() == 0) {
             reload();
         }
-        return stackList.get(1).poll();
+        return out.poll();
     }
 
     public void push(T value) {
-        stackList.get(0).push(value);
+        in.push(value);
     }
 
     private void reload() {
-        while (stackList.get(0).getSize() > 0) {
-            stackList.get(1).push(stackList.get(0).poll());
+        while (in.getSize() > 0) {
+            out.push(in.poll());
         }
     }
 }
