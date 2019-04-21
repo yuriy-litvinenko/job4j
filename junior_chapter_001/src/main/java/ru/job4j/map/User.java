8bf0c9f309b814483c8360bfb2be0e11fc9e1b01
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-04-21 13:23:08 +0600

    1. Создать модель User[#131968]

---
+++ junior_chapter_001/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,39 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+}
