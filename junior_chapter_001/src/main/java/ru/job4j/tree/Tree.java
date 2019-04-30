8657c9b1c4ea04e13c8e17bd21d2a9a78d91e9d7
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-04-30 20:44:44 +0600

    1. Создать элементарную структуру дерева[#131981]

---
+++ junior_chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,76 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+    private Node<E> root;
+
+    Tree(E value) {
+        this.root = new Node<>(value);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        boolean isAvailable = false;
+        Optional<Node<E>> optionalNode = findBy(parent);
+        if (optionalNode.isPresent()) {
+            for (E e : this) {
+                if (e == child) {
+                    isAvailable = true;
+                }
+            }
+            if (!isAvailable && child != null) {
+                optionalNode.get().add(new Node<>(child));
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(root);
+
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return !data.isEmpty();
+            }
+
+            @Override
+            public E next() {
+                Node<E> el;
+                if (hasNext()) {
+                    el = data.poll();
+                    if (el != null) {
+                        for (Node<E> child : el.leaves()) {
+                            data.offer(child);
+                        }
+                        return el.getValue();
+                    }
+                }
+                throw new NoSuchElementException();
+            }
+        };
+    }
+}
