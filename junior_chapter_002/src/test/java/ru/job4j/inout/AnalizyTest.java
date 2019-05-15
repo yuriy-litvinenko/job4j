9c50717c0677e258e32260489e049407e881a5ec
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-05-16 01:38:34 +0600

    Изменены пути к ресурсам в тестах Добавлен параметр jdk в .travis.yml Изменено расширение server.log на server.txt

--- junior_chapter_002/src/test/java/ru/job4j/inout/AnalizyTest.java
+++ junior_chapter_002/src/test/java/ru/job4j/inout/AnalizyTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -13,7 +16,7 @@ public class AnalizyTest {
     @Test
     public void writeShutdownTimeFromLogToFileThenReadIt() {
         Analizy analizy = new Analizy();
-        String source = "src\\main\\resources\\server.log";
+        String source = URLDecoder.decode(Objects.requireNonNull(Config.class.getClassLoader().getResource("server.txt")).getPath(), StandardCharsets.UTF_8);
         String target = "target\\unavailable.csv";
         analizy.unavailable(source, target);
         try (BufferedReader readerTarget = new BufferedReader(new FileReader(target))) {
--- junior_chapter_002/src/test/java/ru/job4j/inout/ConfigTest.java
+++ junior_chapter_002/src/test/java/ru/job4j/inout/ConfigTest.java
@@ -2,6 +2,11 @@ package ru.job4j.inout;
 
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -9,7 +14,8 @@ public class ConfigTest {
 
     @Test
     public void loadParamsFromFileAndGetThem() {
-        Config config = new Config("src\\main\\resources\\app.properties");
+        String path = URLDecoder.decode(Objects.requireNonNull(Config.class.getClassLoader().getResource("app.properties")).getPath(), StandardCharsets.UTF_8);
+        Config config = new Config(path);
         config.load();
         assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
         assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
