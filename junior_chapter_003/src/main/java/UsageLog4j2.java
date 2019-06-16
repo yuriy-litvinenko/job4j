64d80ba5ddedd2466d0bdeb0393b6cb01e290df5
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-06-16 23:04:24 +0600

    4.1. Log4j 2. Логирование системы.[#131921]

---
+++ junior_chapter_003/src/main/java/UsageLog4j2.java
@@ -0,0 +1,13 @@
+import org.apache.log4j.Logger;
+
+public class UsageLog4j2 {
+    private static final Logger LOG = Logger.getLogger(UsageLog4j2.class);
+
+    public static void main(String[] args) {
+        LOG.debug("debug"); // all
+        LOG.info("info"); // except debug
+        LOG.warn("warn"); // except debug and info
+        LOG.error("error"); // except debug, info and warn
+        LOG.fatal("fatal"); // only fatal
+    }
+}
