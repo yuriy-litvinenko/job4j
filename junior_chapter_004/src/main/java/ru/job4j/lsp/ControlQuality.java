788e4873752f6f4fc30bf0bec03946594246c70e
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2019-08-31 02:43:43 +0600

    1. Хранилище продуктов[#132011]

---
+++ junior_chapter_004/src/main/java/ru/job4j/lsp/ControlQuality.java
@@ -0,0 +1,33 @@
+package ru.job4j.lsp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ControlQuality {
+
+    private List<Store> storage = new ArrayList<>();
+
+    boolean putFood(Food food) {
+        boolean result = false;
+        for (Store store : storage) {
+            if (store.accept(food) && store.isNotFull()) {
+                store.put(food);
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    void addWarehouse(int size) {
+        storage.add(new Warehouse(size));
+    }
+
+    void addShop(int size) {
+        storage.add(new Shop(size));
+    }
+
+    void addTrash(int size) {
+        storage.add(new Trash(size));
+    }
+}
