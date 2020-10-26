b5f1ac58816eda177424989668b42e727c9f618b
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2020-10-26 17:29:29 +0600

    1. Запуск нити. Thread#start() [#258515]

---
+++ 03_middle/middle_chapter_01/src/main/java/ru/job4j/threads/ConcurrentOutput.java
@@ -0,0 +1,19 @@
+package ru.job4j.threads;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread third = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        third.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
