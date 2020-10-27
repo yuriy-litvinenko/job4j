3b3deff6516728b84102e7718ee1d3da2158053c
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2020-10-27 21:00:27 +0600

    1.1. Состояние нити. [#258513]

---
+++ 03_middle/middle_chapter_01/src/main/java/ru/job4j/threads/ThreadState.java
@@ -0,0 +1,39 @@
+package ru.job4j.threads;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                    try {
+                        Thread.sleep(15);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    try {
+                        Thread.sleep(15);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        printStatus(first);
+        printStatus(second);
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            printStatus(first);
+            printStatus(second);
+        }
+        printStatus(first);
+        printStatus(second);
+        System.out.println("All threads are terminated");
+    }
+
+    private static void printStatus(Thread thread) {
+        System.out.println(thread.getName() + ' ' + thread.getState());
+    }
+}
