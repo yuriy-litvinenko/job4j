313ee360c9fb2cb652fdc2ec7de30ebe90dd2d22
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2020-10-29 18:51:07 +0600

    1.2. Режим ожидания. [#258514]

---
+++ 03_middle/middle_chapter_01/src/main/java/ru/job4j/threads/ThreadSleep.java
@@ -0,0 +1,19 @@
+package ru.job4j.threads;
+
+public class ThreadSleep {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int index = 0; index <= 100; index++) {
+                            Thread.sleep(250);
+                            System.out.print("\rLoading : " + index  + "%");
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
